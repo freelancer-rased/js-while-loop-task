0c9c0a79c6f5a5b49b8e4a83770d91a158291506
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-02-09 18:00:13 +0600

    even number created

---
+++ display-multipleTime.js
@@ -0,0 +1,7 @@
+// Write a program that prints "I will practice coding every day!" exactly 50 times using a while loop.
+
+let i = 1;
+while (i <= 50){
+    console.log(i, "I will practice coding every day!");
+    i++
+}
